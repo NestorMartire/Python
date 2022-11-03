e5724ac43840d233166dba1cc21d19a66dc05f6a
NestorMartire <110723769+NestorMartire@users.noreply.github.com>
2022-11-03 11:01:02 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def sumar (a, b):
+    print(a + b)
+
+def restar (a, b):
+    print(a - b)
+
+def multiplicarar (a, b):
+    print(a * b)
+
+def dividir (a, b):
+    print(a / b)
+
---
+++ nuevo.py
@@ -0,0 +1,5 @@
+import calculadora as c
+c.dividir (4, 5)
+c.multiplicarar (3,8)
+c.restar (2, 7)
+c.sumar (9, 3)
